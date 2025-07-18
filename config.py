03d114c56fb2669356cbb8dfabd7fcafd9766f4e
parasgodhani01 <godhaniparas44@gmail.com>
2025-07-18 18:50:18 -0500

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-def process_pdf():
+from PyPDF2 import PdfReader
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI , GoogleGenerativeAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class process_pdf():
     pass
