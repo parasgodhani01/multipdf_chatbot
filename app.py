3c7c1b3d6608045ce94edfcd8ddcbceac9046e75
parasgodhani01 <godhaniparas44@gmail.com>
2025-07-18 17:51:56 -0500

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Herllo")
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import google.generativeai as genai 
+from langchain_community.vectorstores import FAISS #facebook ai index search
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+
+from config import process_pdf
+
+load_dotenv(override=True)
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
