aee18c54b5f5eec5e2a0b8ac46bbc6ed13c54680
parasgodhani01 <godhaniparas44@gmail.com>
2025-07-19 15:26:47 -0500

    config.py

--- config.py
+++ config.py
@@ -6,4 +6,39 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class process_pdf():
-    pass
+    
+    def get_pdf_text(pdf_docs):
+        text = ""
+        for pdf in pdf_docs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text = text + page.extract_text()
+        return text
+    
+    def get_text_chuks(text):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000,chunk_overlap = 1000)
+        chunks = text_splitter.split_text(text)
+        return chunks
+    
+    def get_vector_store(text_chunks):
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        vector_store = FAISS.from_texts(text_chunks,embedding=embeddings)
+        vector_store.save_local("faisee_index")
+    
+    def conversational_chain():
+        prompt_template = """
+        Answer the question as detailed as possible from the provided context, make sure to provide all the details, 
+        if the answer is not in provided context just say, "answer is not available in the context", 
+        don't provide the wrong answer\n\n
+        Context:\n {context}?\n
+        Question: \n{question}\n
+
+        Answer:
+        """
+
+        model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2)
+
+        prompt = PromptTemplate(template=prompt_template, input_variables=["context","question"])
+        chain = load_qa_chain(model,chain_type="stuff",prompt=prompt)
+
+        return chain
